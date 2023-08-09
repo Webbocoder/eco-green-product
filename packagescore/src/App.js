c938e0fc3f175a79abd57997243b825b99d55096
pasujemito <damian.janik@iceland.co.uk>
2023-08-09 11:28:10 +0100

    adds product list

--- packagescore/src/App.js
+++ packagescore/src/App.js
@@ -1,24 +1,17 @@
 import logo from './logo.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import data from './data/data.json'
+
+import {Header} from './components/Header'
+import {Search} from './components/Search'
+import {ProductList} from './components/ProductList'
+import {Basket} from './components/Basket'
 
 function App() {
   return (
     <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
+      <ProductList data={data}/>
     </div>
   );
 }
