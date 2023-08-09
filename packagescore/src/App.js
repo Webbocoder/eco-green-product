0837164bea055c6f3a103804a341955720a4dcfd
nick v <nvalente.electrics@gmail.com>
2023-08-09 12:05:52 +0100

    added props to app to basket

--- packagescore/src/App.js
+++ packagescore/src/App.js
@@ -2,15 +2,17 @@ import logo from "./logo.svg";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import data from "./data/data.json";
+import { useState } from 'react';
 
 import Header from './components/Header'
 import Search from './components/Search'
 import ProductList from './components/ProductList'
 import Basket from './components/Basket'
+import React from "react";
 
 function App() {
 
-  const [basket, setBasket] = useState(["name"= "banana", "price"= "5"])
+  const [basket, setBasket] = useState ([{"name":"banana", "price":"5"}])
 
   return (
     <div className="App">
