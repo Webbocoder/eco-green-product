7deb4c0339481ecf98289a5fe86620c34a11c6f2
Andrew <andrew@eseeds.com>
2023-08-09 11:36:51 +0100

    Header component data

--- packagescore/src/components/Header.js
+++ packagescore/src/components/Header.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import Twitter from './Images/twitter.png';
+import Pinterest from './Images/pinterest.png';
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <>
+      <div>
+        <p>Logo</p>
+        <p>Iceland</p>
+      </div>
+      <div>
+        <button>Help</button>
+        <button>Store Finder</button>
+        <img src={Twitter} alt='old twitter logo' />
+        <img src={Pinterest} alt='pinterest logo' />
+      </div>
+    </>
+  );
 }
 
 export default Header;
